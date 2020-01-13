da5414be9feeb96430ae386c318983eee2e5c857
Anastasia Tsyagnok <anastasia.t@kultprosvet.net>
2020-01-13 13:01:11 +0200

    OBR-4 add types and actions functionality

--- src/ReduxHelper.ts
+++ src/ReduxHelper.ts
@@ -1,43 +1,52 @@
+import {Record} from 'immutable'
+
+type RecursivePartial<T> = {
+    [P in keyof T]?:
+    T[P] extends (infer U)[] ? RecursivePartial<U>[] :
+        T[P] extends object ? RecursivePartial<T[P]> :
+            T[P];
+};
+
 export const ReduxHelper = {
-    getActionTypes(data) {
+    // keys<T>(data:T):[keyof T]{
+    //     return Object.keys(data)
+    // },
+    generateActions<T>(data:T, store) {
         const types = Object.keys(data)
-        const actionTypes = <string[]> []
-        types.forEach((type:string) => {
-            actionTypes.push(type.toUpperCase() + '_UPDATE')
-            actionTypes.push(type.toUpperCase() + '_RESET')
-        })
-        return actionTypes
-    },
-
-    generateActions(data) {
-        const types = Object.keys(data)
-        const actions = {}
-        types.forEach(type => {
-
-            actions['update'] = (type:string, payload:any) => {
-                if (!types.includes(type)) {
+        return  {
+            update<K extends keyof T>(type: K, payload:RecursivePartial<T[K]>){
+                if (!types.includes(type.toString())) {
                     throw Error ('Type does not exist')
                     return
                 }
 
-                if (!payload) {
-                    throw Error ('Please provide payload')
+                if (typeof payload !== "object" || payload == null) {
+                    throw Error ('Payload must be an object')
                     return
                 }
+
+                store.dispatch({
+                    type: `${type.toString().toUpperCase()}_UPDATE`,
+                    payload
+                })
+            },
+
+            reset(type) {
+                store.dispatch({
+                    type: `${type.toUpperCase()}_UPDATE`
+                })
             }
+        }
 
-            actions['reset'] = (type) => {}
-
-        })
-        return actions
     },
 
     createReducers(data) {
         let reducers:any={}
         for (let key in data){
-            reducers[key] = function (state = data[key], action) {
+            const initialState = Record(data[key])
+            reducers[key] = function (state = new initialState(data[key]), action) {
                 if(action.type==`${key.toUpperCase()}_UPDATE`) {
-                    return {...state, ...action.payload}
+                    return state.mergeDeep(action.payload)
                 } else if(action.type==`${key.toUpperCase()}_RESET`){
                     return data[key]
                 }
@@ -47,5 +56,26 @@ export const ReduxHelper = {
         return reducers
     },
 }
-
+// export const data = {
+//     user: {
+//         name: 'John',
+//         age: 20
+//     },
+//     posts: {
+//         first: {
+//             title: {
+//                 en: 'Title',
+//                 ru: 'Заголовок'
+//             },
+//             text: 'Lorem ipsum'
+//         },
+//         count: 15
+//     }
+// }
+// let actions=ReduxHelper.generateActions(data,{})
+// actions.update("posts", {first: {
+//     title: {
+//         en: 'FASDGDFG'
+//     }
+// }})
 
